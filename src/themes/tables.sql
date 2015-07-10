4e08172e840d80b5a5619835720db485c7283962
magdakosior <magda.kosior@tesera.com>
2015-07-10 15:33:53 -0600

    updates schema naming

--- src/themes/tables.sql
+++ src/themes/tables.sql
@@ -1,54 +1,67 @@
-CREATE SCHEMA themes;
+CREATE SCHEMA datathemes_mackenzie;
+CREATE SCHEMA mackenzie_sonde;
+CREATE SCHEMA mackenzie_dgt;
+CREATE SCHEMA mackenzie_grab;
+CREATE SCHEMA mackenzie_sonde;
 
-CREATE TABLE IF NOT EXISTS themes.themes_meta (
-	theme_name text,
+CREATE TABLE IF NOT EXISTS datathemes.metadata (
+    theme_name text,
     theme_version text,
-	repository text,
-	author text,
-	description text,
-	schemas text,
-	datetime_created timestamp DEFAULT now(),
-    datetime_modified timestamp DEFAULT now(),
+    repository text,
+    author text,
+    description text,
+    schemas text,
+    is_published boolean NOT NULL DEFAULT false,
+    datetime_created timestamp DEFAULT now(),
+    datetime_modified timestamp DEFAULT now()
     CONSTRAINT themes_meta_pkey PRIMARY KEY (
-        theme_name) );
+        theme_name,
+        is_published) );
 
-CREATE TABLE IF NOT EXISTS themes.sonde_data (
-	upload_id text,
-	source_row_index int,
+CREATE TABLE IF NOT EXISTS mackenzie_sonde.data (
+    upload_id text,
+    source_row_index int,
     theme_name text,
     site text,
-	date_in timestamp,
-	temp numeric,
-	spCond numeric,
-	pH numeric,
-	orp numeric,
-	turbid+ numeric,
-	chl numeric,
-	odoSat numeric,
-	odo numeric,
-	datetime_created timestamp DEFAULT now(),
-    datetime_modified timestamp DEFAULT now(),
+    date_in timestamp,
+    temp numeric,
+    sp_cond numeric,
+    ph numeric,
+    orp numeric,
+    turbid numeric,
+    chl numeric,
+    odo_sat numeric,
+    odo numeric,
+    is_committed boolean NOT NULL DEFAULT false,
+    datetime_created timestamp DEFAULT now(),
+    datetime_modified timestamp DEFAULT now()
     CONSTRAINT sonde_data_pkey PRIMARY KEY (
         upload_id,
         theme_name,
-        site) );
+        site,
+        is_committed) );
 
-CREATE TABLE IF NOT EXISTS themes.sonde_meta (
-	upload_id text,
-	theme_name text,
+CREATE TABLE IF NOT EXISTS mackenzie_sonde.metadata (
+    upload_id text,
+    source_row_index int,
+    theme_name text,
     extents text,
-	qa-qc text,
-	equipment text,
-	sampler text,
-	lab text,
-	depth numeric,
-	matrix text,
-	related text,
-	type text,
-	series text,
-	comments text
-    CONSTRAINT plot_pkey PRIMARY KEY (
+    qa_qc text,
+    equipment text,
+    sampler text,
+    lab text,
+    depth numeric,
+    matrix text,
+    related text,
+    type text,
+    series text,
+    comments text,
+    is_committed boolean NOT NULL DEFAULT false,
+    datetime_created timestamp DEFAULT now(),
+    datetime_modified timestamp DEFAULT now()
+    CONSTRAINT sonde_meta_pkey PRIMARY KEY (
         upload_id,
         theme_name,
-        matrix ) );
+        matrix,
+        is_committed ) );
 
